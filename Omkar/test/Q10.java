bef5c0bd6a4a56bdcc9520087d14ce1384b456ce
coderomi <sontakkeomkar5@gmail.com>
2024-08-28 21:23:28 +0530

    AutoboxingUnboxingExp
    AutoboxingUnboxingExp  done

--- Omkar/test/Q10.java
+++ Omkar/test/Q10.java
@@ -1,13 +1,13 @@
 /*
-Method Overloading :-Same method name, different parameters.
-To provide multiple ways to use a method.
-Scope Within a single class.
-Polymorphism Not used for runtime polymorphism.
-
-
-Method Overriding:-	Same method name and parameters in subclass.
-Parameters	Must be the same as the superclass method.
-Scope Between superclass and subclass.
-Polymorphism Used for runtime polymorphism.
-
-*/
+ * Method Overloading :-Same method name, different parameters.
+ * To provide multiple ways to use a method.
+ * Scope Within a single class.
+ * Polymorphism Not used for runtime polymorphism.
+ * 
+ * 
+ * Method Overriding:- Same method name and parameters in subclass.
+ * Parameters Must be the same as the superclass method.
+ * Scope Between superclass and subclass.
+ * Polymorphism Used for runtime polymorphism.
+ * 
+ */
